90dbda5000ea083062f491f04c27ea194751a1ad
RdEl00 <elaslireda35@gmail.com>
2022-10-18 23:36:38 +0000

    update

--- printf.c
+++ printf.c
@@ -5,40 +5,59 @@
  *
  */
 
+
 int _printf(const char *format, ...)
 {
-	types prinf[] = {
+	print_f printf[] = {
 		{"c", printc},
 		{NULL, NULL}
 	};
 
-	unsigned int i, j;
-
 	va_list list;
-
+	unsigned int i = 0, j = 0;
 	va_start(list, format);
-
 	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			for (j = 0; printf[j].p != NULL; j++)
-			{
-				if (format[i + 1] == printf[j].p[0])
-				{
-					printf[j].func(list);
-					i++;
-				}
-			}
-		}
-
-		else
-		{
-			_putchar(format[i]);
-		}
-	
-	}
-	va_end(list);
-	return (0);
-
+        {
+                if (format[i] == '%')
+                {
+                  if (format[i + 1] == '%')
+                  {
+                    _putchar('%');
+                    i++;
+                  }
+                  else if (comp_f(format, i + 1) != NULL)
+                  {
+                    j += (comp_f(format, i + 1))(list);
+                  }
+                  else
+                  {
+                    _putchar(format[i]);
+                  }
+                }
+        }
 }
+
+int (*comp_f(const char *a, int ubc))
+{
+  print_f pu[] = {
+    {"c", printc},
+    {NULL, NULL}};
+
+    unsigned int k;
+    for (k = 0, pu[k].p != NULL, k++)
+    {
+      if (pu[k].p[0] == a[ubc])
+        {
+          return (pu[k].func);
+        }
+    }
+    return (NULL);
+}
+
+int main(void)
+{
+
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
