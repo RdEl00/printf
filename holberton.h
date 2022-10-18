1d898a46277c7c59eb07258f99d165cab57556b3
RdEl00 <elaslireda35@gmail.com>
2022-10-18 19:29:18 +0000

    Add struct - Header

--- holberton.h
+++ holberton.h
@@ -1,6 +1,24 @@
 #ifndef Holberton_h
 #define Holberton_h
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 int _printf(const char *format,...);
+int printc(va_list l);
+int _putchar(char c);
+
+/**
+ * struct types - specifier structure for printf
+ * @p: pointer to characters specifiers
+ * @func: function pointer to print functions
+ */
+
+typedef struct types
+{
+	char *p;
+	int (*func)(va_list);
+}types;
 
 #endif /*Holberton_h*/
