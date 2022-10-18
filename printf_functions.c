3a5485505b656b10d4a9ce61e9f808e74519565c
RdEl00 <elaslireda35@gmail.com>
2022-10-18 19:43:39 +0000

    prinf functions

---
+++ printf_functions.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _putchar - Entry function
+ * @c: variable
+ * return: print
+ */
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));	
+}
+/**
+ * printc - Entry function
+ * @list: variable
+ * return: integer
+ */
+
+int printc(va_list list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}	
