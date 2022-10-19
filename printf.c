a57e09a82482486b89c02add17a2d3d1a626442d
RdEl00 <elaslireda35@gmail.com>
2022-10-19 00:00:52 +0000

    main file

--- printf.c
+++ printf.c
@@ -1,63 +1,68 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _prinf - printf function
+ * @format: variable
+ * return: 0
  */
 
 
 int _printf(const char *format, ...)
-{
-	print_f printf[] = {
-		{"c", printc},
-		{NULL, NULL}
-	};
 
 	va_list list;
 	unsigned int i = 0, j = 0;
-	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
-        {
-                if (format[i] == '%')
-                {
-                  if (format[i + 1] == '%')
-                  {
-                    _putchar('%');
-                    i++;
-                  }
-                  else if (comp_f(format, i + 1) != NULL)
-                  {
-                    j += (comp_f(format, i + 1))(list);
-                  }
-                  else
-                  {
-                    _putchar(format[i]);
-                  }
-                }
-        }
+
+int _printf(const char *format, ...)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				i++;
+				j++;
+			}
+			else if (cmp_func(format, i + 1) != NULL)
+			{
+				j += (cmp_func(format, i + 1))(list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				j++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
+	}
+	va_end(list);
+	return (j);
 }
 
-int (*comp_f(const char *a, int ubc))
+/**
+ * cmp_func - Entry point
+ * @a: Pointer
+ * @ubc: integer
+ *
+ * Return: 0.
+ */
+int (*cmp_func(const char *a, int ubc))(va_list)
 {
-  print_f pu[] = {
-    {"c", printc},
-    {NULL, NULL}};
+	print_f printf[] = {
+		{"c", printc},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	int k;
 
-    unsigned int k;
-    for (k = 0, pu[k].p != NULL, k++)
-    {
-      if (pu[k].p[0] == a[ubc])
-        {
-          return (pu[k].func);
-        }
-    }
-    return (NULL);
+	for (k = 0; printf[k].p != NULL; k++)
+	{
+		if (printf[k].p[0] == a[ubc])
+		{
+			return (printf[k].func);
+		}
+	}
+	return (NULL);
 }
-
-int main(void)
-{
-
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
